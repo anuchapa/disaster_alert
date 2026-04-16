4b0291e3a235dcd99dd2bb5e87f3713d7dd6a6d2
anuchapa <anuchapa.thongkam@gmail.com>
2026-04-16 16:00:05 +0700

    update connection string

--- DisasterAlert.context/Migrations/20260416083916_UpdateDataTable160426T2.Designer.cs
+++ DisasterAlert.context/Migrations/20260416083916_UpdateDataTable160426T2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using DisasterAlert.context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace DisasterAlert.context.Migrations
 {
     [DbContext(typeof(DisasterAlertContext))]
-    partial class DisasterAlertContextModelSnapshot : ModelSnapshot
+    [Migration("20260416083916_UpdateDataTable160426T2")]
+    partial class UpdateDataTable160426T2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
